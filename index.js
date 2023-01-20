4a52ad3ed1b072ebe1527d1a450b712fa0d8bb83
Hanal <anupirlo999@gmail.com>
2023-01-20 03:48:21 -0800

    commit

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 const express = require("express");
+// const dotenv = require("dotenv");
+// const connectDB = require("./config/db");
+// const userRouter = require('./Routes/userRouter')
+const app =express();
 
-const app = express()
+app.use(express.json()) ;
+
+// dotenv.config();
+// connectDB()
+
+// app.use('/api/user',userRouter);
+
+
+
+// app.use(notFound) 
+// app.use(errorHandler)
 
 app.get("/", (req, res) => {
-    res.json("API is Success..");
+    res.send("API is running..");
   });
 
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,console.log(`server start ${PORT}`)); 
-
-
+app.listen(PORT,console.log(`server start ${PORT}`));
